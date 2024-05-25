a5235f66dc92f98665e79d9acb52f3147e8c4e3b
momotan <momotaha039@gmail.com>
2024-05-25 16:34:09 +0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+function Header() {
+    const [showMobileMenu, SetShowMobileMenu] = useState(false)
+  return (
+    <header >
+            <nav id="main-menu">
+                <button onClick={()=>SetShowMobileMenu(!showMobileMenu)} id="btnBarMenu"><i className={`fa ${showMobileMenu?'fa-close':'fa-bars'}`}></i></button>
+                <ul className={showMobileMenu?'show':''}>
+                    <a onClick={()=>SetShowMobileMenu(false)} href="#about">
+                        <li>About</li>
+                    </a>
+                    <a onClick={()=>SetShowMobileMenu(false)} href="#skills">
+                        <li>Skills</li>
+                    </a>
+                    <a onClick={()=>SetShowMobileMenu(false)} href="#projects">
+                        <li>Projects</li>
+                    </a>
+                    <a onClick={()=>SetShowMobileMenu(false)} href="#contact">
+                        <li>Contact</li>
+                    </a>
+                </ul>
+            </nav>
+        </header>
+  )
+}
+
+export default Header
