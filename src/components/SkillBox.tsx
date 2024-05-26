a02aeac60ea370a57cf4a2ea575b25bdc83e5a54
momotan <momotaha039@gmail.com>
2024-05-26 15:09:02 +0300

    create skill box for skills

---
+++ src/components/SkillBox.tsx
@@ -0,0 +1,30 @@
+import { ReactNode } from "react"
+
+  export interface skillType{
+    title:string,
+    faIconClass?:string,
+    image?:string
+}
+export interface skillBoxType{
+    term:string,
+    skills:skillType[]
+}
+
+function SkillBox({skillsBox}:{children?:ReactNode,skillsBox:skillBoxType}) {
+  return (
+    <div className="skill-box">
+          <h1><i className="fas fa-mobile-alt"></i>{skillsBox.term}</h1>
+          {
+            skillsBox.skills.map(skill => {
+                return <span className="skill"><span className="percent">100%</span>
+                            {skill.faIconClass&&<i className={skill.faIconClass}></i>}
+                            {skill.image&&<img width="38px" height="38px" src={skill.image}/>}
+                            {skill.title}
+                        </span>
+            })
+          }
+      </div>
+  )
+}
+
+export default SkillBox
