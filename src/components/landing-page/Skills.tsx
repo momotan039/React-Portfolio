76f35c6b52a66e1d1c453739e58632ed96a57420
momotan <momotaha039@gmail.com>
2024-05-26 15:09:19 +0300

    call skillsBox and reduce the code

--- src/components/landing-page/Skills.tsx
+++ src/components/landing-page/Skills.tsx
@@ -1,35 +1,101 @@
+import SkillBox, { skillBoxType } from "../SkillBox"
+
 function Skills() {
+  const frontEndSkills:skillBoxType=
+    {
+      term:'Front End',
+      skills:[
+        {
+          title:'Html5',
+          faIconClass:'fab fa-html5'
+        },
+        {
+          title:'Css3',
+          faIconClass:'fab fa-css3-alt'
+        },
+        {
+          title:'Java Script',
+          faIconClass:'fab fa-js'
+        },
+      ]
+    }
+    const backEndSkills:skillBoxType=
+    {
+      term:'BackEnd',
+      skills:[
+        {
+          title:'.NET',
+          faIconClass:'fa-brands fa-adn'
+        },
+        {
+          title:'C#',
+          faIconClass:'fas fa-code'
+        },
+        {
+          title:'Php',
+          faIconClass:'fab fa-php'
+        },
+        {
+          title:'Node.js',
+          faIconClass:'fab fa-node-js'
+        },
+      ]
+    }
+    const frameWorksAndLibraries:skillBoxType=
+    {
+      term:'Frameworks & Libraries',
+      skills:[
+        {
+          title:'.NET',
+          faIconClass:'fa-brands fa-adn'
+        },
+        {
+          title:'Angular',
+          faIconClass:'fab fa-angular'
+        },
+        {
+          title:'React.js',
+          faIconClass:'fab fa-react'
+        },
+        {
+          title:'Next.js',
+          faIconClass:'fa-solid fa-code'
+        },
+        {
+          title:'Redux',
+          image:'images/redux.png'
+        },
+        {
+          title:'Bootstrap',
+          faIconClass:'fa-brands fa-bootstrap'
+        },
+        {
+          title:'JQuery',
+          faIconClass:'fa-solid fa-code'
+        },
+      ]
+    }
+    const mobileDevSkills:skillBoxType={
+        term:'Mobile Development',
+        skills:[
+          {
+            title:'Android',
+            faIconClass:'fab fa-android'
+          },
+          {
+            title:'Flutter',
+            image:'images/flutter.png'
+          }
+        ]
+      }
   return (
     <section id="skills" className="skills-section container">
-                
     <h1 className="title">Skills</h1>
-    {/* <ArrowLinks elementId='#projects'/> */}
   <div className="content">
-      <div className="skill-box">
-       <h1><i className="fas fa-laptop-code"></i>Front End</h1>
-       <span className="skill"><span className="percent">100%</span><i className="fab fa-html5"></i>Html</span>
-       <span className="skill"><span className="percent">100%</span><i className="fab fa-css3-alt"></i>Css</span>
-       <span className="skill"><span className="percent">100%</span><i className="fab fa-js"></i>Java Script</span>
-       <span className="skill"><span className="percent">100%</span><img width="38px" height="38px" src="images/redux.png"/>Redux</span>
-      </div>
-      <div className="skill-box">
-          <h1><i className="fab fa-free-code-camp"></i>Back End</h1>
-          <span className="skill"><span className="percent">100%</span><i className="fas fa-code"></i>C#</span>
-          <span className="skill"><span className="percent">100%</span><i className="fab fa-php"></i>Php</span>
-          <span className="skill"><span className="percent">100%</span><i className="fab fa-node-js"></i>Node</span>
-      </div>
-      <div className="skill-box">
-          <h1><i className="fas fa-stream"></i>Frameworks & Libraries</h1>
-          <span className="skill"><span className="percent">100%</span><i className="fas fa-code"></i>.netCore</span>
-          <span className="skill"><span className="percent">100%</span><i className="fab fa-angular"></i>Angular</span>
-          <span className="skill"><span className="percent">100%</span><i className="fab fa-react"></i>React.js</span>
-          <span className="skill"><span className="percent">100%</span><i className="fa-solid fa-code"></i>Next.js</span>
-      </div>
-      <div className="skill-box">
-          <h1><i className="fas fa-mobile-alt"></i>Mobile Development</h1>
-          <span className="skill"><span className="percent">100%</span><i className="fab fa-android"></i>Android</span>
-          <span className="skill"><span className="percent">100%</span> <img width="38px" height="38px" src="images/flutter.png"/> Flutter</span>
-      </div>
+      <SkillBox skillsBox={frontEndSkills}/>
+      <SkillBox skillsBox={backEndSkills}/>
+      <SkillBox skillsBox={frameWorksAndLibraries}/>
+      <SkillBox skillsBox={mobileDevSkills}/>
   </div>
 </section>
   )
